c477f5701d55fc355f715b5cc903a6b7030ba552
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2019-03-20 21:29:57 -0500

    add usage report file

---
+++ sms-usage-report.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws s3 sync s3://<your-S3-bucket>/SMSUsageReports/us-west-2/2019/02 . && find . –name "*.gz" | xargs gunzip
+TOTALSMS=$(find . -type d | cat  / | awk -F ',' '{count[$8]++} END {for (word in count) print word, count[word]}' | grep -v  TotalParts | awk -F " " '{print $2}')
+echo "Total SMS sent: ${TOTALSMS}"
